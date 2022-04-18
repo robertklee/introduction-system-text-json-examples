f9a002b71fadee32f4db30dc9c3c3c1f181d83cc
Robert Lee <robertlee@microsoft.com>
2022-04-18 10:04:46 -0700

    add tests

--- JsonExampleProject1/JsonExampleProject1/Program.cs
+++ JsonExampleProject1/JsonExampleProject1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
@@ -10,15 +11,185 @@ namespace JsonExampleProject1
 { 
 	class Program
 	{
+		private static readonly JsonSerializerOptions JsonOptionsAllowComments = new JsonSerializerOptions()
+		{
+			ReadCommentHandling = JsonCommentHandling.Skip,
+			AllowTrailingCommas = true,
+		};
+
+		public class TestObject
+		{
+			public DateTime Date { get; set; }
+			public int Int { get; set; }
+			public string String { get; set; }
+			public bool Boolean { get; set; }
+			public long Long { get; set; }
+			public double Double { get; set; }
+			public double[] DoubleArr { get; set; }
+			public int[] IntArr { get; set; }
+			public string[] StringArr { get; set; }
+		}
+
 		private static async Task Main()
 		{
-			SerializeExample();
-			await SerializeToFile();
-			DeserizalizeExample();
-			DeserializeWithJsonDocument();
-			SerializeWithOptions();
-			DeserizalizeWithOptions();
+			RunTests();
+
+            //SerializeExample();
+            await SerializeToFile();
+            //DeserizalizeExample();
+            //DeserializeWithJsonDocument();
+            //SerializeWithOptions();
+            //DeserizalizeWithOptions();
+        }
+
+		private static void RunTests()
+        {
+			//// Float tests
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": 5.0}")); 
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": Infinity}")); // Throws
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": -Infinity}")); // Throws
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": NaN}")); // Throws
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": ""Infinity""}")); 
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": ""-Infinity""}")); 
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": ""NaN""}"));
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": ""INF""}")); 
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": ""-INF""}")); 
+
+			//// Trailing commas
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": 5.0,}")); 
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": 5.0,,}")); // Throws
+
+			//// Trailing commas in arrays
+			//Debug.WriteLine(DeserializeTestJson(@"{""IntArr"": [5, 10, 15,]}"));
+			//Debug.WriteLine(DeserializeTestJson(@"{""IntArr"": [5, 10, 15,,]}")); // Throws
+
+			//// Extra commas between two tokens
+			//Debug.WriteLine(DeserializeTestJson(@"{""Double"": 5.0,, ""Int"": 10}")); // Throws
+			//Debug.WriteLine(DeserializeTestJson(@"{""IntArr"": [5, 10,, 15]}")); // Throws
+
+			//// Comments inside arrays (ALL THROWS)
+			//			Debug.WriteLine(DeserializeTestJson($@"
+			//""StringArr"": [""hi"", //comment
+			//""second line""]
+			//", JsonOptionsAllowComments));
+
+			//			Debug.WriteLine(DeserializeTestJson($@"
+			//""StringArr"": [""hi"",
+			////
+			//""second line""]
+			//", JsonOptionsAllowComments));
+
+			//			Debug.WriteLine(DeserializeTestJson($@"
+			//""StringArr"": [""hi"", /*
+			//comment
+			//goes here */
+			//""second line""]
+			//", JsonOptionsAllowComments));
+
+			//// Comments outside arrays
+			// Single-line comments
+			Debug.WriteLine(DeserializeTestJson($@"
+                /* comment */
+                ""String"": ""42 is the meaning of life""
+                ", JsonOptionsAllowComments));
+
+			Debug.WriteLine(DeserializeTestJson($@"
+                //comment*//*hi*/
+                ""String"": ""42 is the meaning of life""
+                ", JsonOptionsAllowComments));
+
+			Debug.WriteLine(DeserializeTestJson($@"
+                ""String"": // comment
+                ""42 is the meaning of life""
+                ", JsonOptionsAllowComments));
+
+			Debug.WriteLine(DeserializeTestJson($@"
+                ""String"": //
+                ""42 is the meaning of life""
+                ", JsonOptionsAllowComments));
+
+			Debug.WriteLine(DeserializeTestJson($@"
+                ""String"": ""42 is the meaning of life"" // comment
+                ", JsonOptionsAllowComments));
+
+			Debug.WriteLine(DeserializeTestJson($@"
+                ""String"": ""42 is the meaning of life"" //* comment */
+                ", JsonOptionsAllowComments));
+
+			// Multi-line comments
+			Debug.WriteLine(DeserializeTestJson($@"
+                /* comments
+                take up
+                multiple lines*/
+                ""String"": ""42 is the meaning of life""
+                ", JsonOptionsAllowComments));
+
+			Debug.WriteLine(DeserializeTestJson($@"""String"": /* comments
+                take up
+                multiple lines*/
+                ""42 is the meaning of life""
+                ", JsonOptionsAllowComments));
+
+			Debug.WriteLine(DeserializeTestJson($@"""String"": /*
+                
+                
+                */
+                ""42 is the meaning of life""
+                ", JsonOptionsAllowComments));
+
 		}
+
+
+		private static bool DeserializeTestJson(string json, JsonSerializerOptions options = default)
+        {
+			if (options == default)
+            {
+				options = new JsonSerializerOptions()
+                {
+					AllowTrailingCommas = true,
+					NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals,
+                };
+            }
+
+			string serializedJson;
+
+            try
+            {
+                var obj = JsonSerializer.Deserialize<TestObject>(json, options);
+                serializedJson = JsonSerializer.Serialize(obj, options);
+			}
+            catch (JsonException e)
+            {
+				Debug.WriteLine(e.Message);
+
+				return false;
+            }
+
+			return true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		private static void SerializeExample()
 		{
